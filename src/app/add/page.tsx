ce7643f69247f46fb5d9a392cf88f45addea6816
mgpark <dev.mgpark@gmail.com>
2024-07-23 15:29:28 +0900

    modify layout component

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,8 +1,7 @@
-import NavBar from "../components/NavBar";
+import NavBar from "@/app/components/NavBar";
 
 const page = () => (
   <div>
-     <NavBar />
      <main>
 		    <h1>New Bookmark Form</h1>
 		 </main>   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import NavBar from "@/app/components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <main>
         <h1>Welcome to Bookmarker</h1>
       </main>
