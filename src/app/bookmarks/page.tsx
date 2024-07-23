f7a6b7cfdb055a3415d8c98ee1523bdeeecbd5cd
mgpark <dev.mgpark@gmail.com>
2024-07-23 15:50:49 +0900

    redirect settings

---
+++ src/app/bookmarks/page.tsx
@@ -0,0 +1,20 @@
+import NavBar from "@/app/components/NavBar";
+
+export default function BookmarkPage()  {
+  const { titles } = getData()
+  
+  return (
+    <div>
+        <h1>Welcome to Bookmarker</h1>
+        {
+          titles.map((title,idx) => <h2 key={idx}>{title}</h2>)
+        }
+    </div>
+  );
+}
+
+function getData()  {
+  return {
+      titles: ['Bookmark1', 'Bookmark2']
+  }
+}
