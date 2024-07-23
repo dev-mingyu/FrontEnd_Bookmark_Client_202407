a0a6a9c919b145ee8fd45de15702348de35c3c80
mgpark <dev.mgpark@gmail.com>
2024-07-23 15:07:42 +0900

    FrontEnd Bookmark First commit

---
+++ src/app/add/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+
+const page = () => (
+  <div>
+     <NavBar />
+     <main>
+		    <h1>New Bookmark Form</h1>
+		 </main>   
+  </div>
+);
+
+export default page;
