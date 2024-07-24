41ff6b637a61672a4816a4a21b9a901ea22f7aaa
mgpark <dev.mgpark@gmail.com>
2024-07-24 09:47:02 +0900

    env 파일 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/bookmarks',
-          permanent: true
-        }
-      ]
-    }
-  };
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/bookmarks',
+        permanent: true
+      }
+    ]
+  },
+  async rewrites() {
+    const API_BASE_URL = process.env.NEXT_PUBLIC_SERVER_API_BASE_URL
+    
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${API_BASE_URL}/api/:path*`
+        //'http://bookmarker-api-svc:8080/:path*/' // Proxy to Backend
+      }
+    ]
+  },
+    // serverRuntimeConfig: {
+    //   API_BASE_URL: process.env.NEXT_PUBLIC_SERVER_API_BASE_URL
+    // },
+    // publicRuntimeConfig: {
+    //   API_BASE_URL: process.env.NEXT_PUBLIC_CLIENT_API_BASE_URL
+    // },
+};
 
 export default nextConfig;
